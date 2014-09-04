806f8b6a82f4cca646df27942cf8395914066893
Scott <dev@misonline.biz>
2014-09-04 19:28:02 -0400

    Updated Distro Files

--- src/elliptical-platform.js
+++ src/elliptical-platform.js
@@ -11350,3 +11350,43 @@ if (document.readyState === 'complete' || scope.flags.eager) {
     return updateBindings(this, name, binding);
   }
 })(this);
+
+/*
+ * =============================================================
+ * elliptical.platform
+ * =============================================================
+ *
+ */
+
+//umd pattern
+
+(function (root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        //commonjs
+        module.exports = factory(require('elliptical-utils'),require('ellipse-utils'),require('ellipsis-touch'),require('ellipsis-animation'),
+            require('ellipsis-element'),require('elliptical-observe'),require('elliptical-nodebind'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['elliptical-utils','ellipse-utils','ellipsis-touch','ellipsis-animation','ellipsis-element',
+            'elliptical-observe','elliptical-nodebind'], factory);
+    } else {
+        // Browser globals (root is window)
+
+        root.returnExports = factory(root.elliptical.utils);
+    }
+}(this, function (utils) {
+
+    $.elliptical= $.elliptical || {};
+    $.elliptical.hasObjectObserve=Observer.hasObjectObserve;
+    $.elliptical.utils=utils;
+    $.elliptical.startDirtyCheck=function(){
+        var interval_=window.__dirtyCheckInterval || 500;
+        console.log('dirty checking started...');
+        var timeoutId=setInterval(function(){
+            Platform.performMicrotaskCheckpoint();
+        },interval_);
+    };
+    return $;
+
+
+}));

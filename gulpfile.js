6459644233b5bebe325287ad047aa6f5f6232a19
Scott <dev@misonline.biz>
2014-09-04 19:04:04 -0400

    Added core gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var gulp=require('gulp'),
     uglify = require('gulp-uglify'),
     merge = require('merge-stream'),
     build=require('./build.json'),
+    buildCore=require('./build-core.json'),
     release=require('./build/dist.json'),
+    releaseCore=require('./build/dist-core.json'),
     src='./src',
     dist='./dist';
 
@@ -14,16 +16,16 @@ var gulp=require('gulp'),
 
 
 gulp.task('default',function(){
-    console.log('elliptical-platform build..."tasks: gulp build|gulp minify"');
+    console.log('elliptical-platform build..."tasks: gulp build|build-core|minify|minify-core"');
 });
 
 gulp.task('build',function(){
 
-    var build_=platformStream()
+    var build_=srcStream(build)
         .pipe(concat('elliptical-platform.js'))
         .pipe(gulp.dest(src));
 
-    var release_=releaseStream()
+    var release_=srcStream(release)
         .pipe(concat('elliptical-platform.js'))
         .pipe(gulp.dest(dist));
 
@@ -31,13 +33,28 @@ gulp.task('build',function(){
 
 });
 
+gulp.task('build-core',function(){
+
+    var build_=srcStream(buildCore)
+        .pipe(concat('elliptical-platform-core.js'))
+        .pipe(gulp.dest(src));
+
+    var release_=srcStream(releaseCore)
+        .pipe(concat('elliptical-platform-core.js'))
+        .pipe(gulp.dest(dist));
+
+    return merge(build_, release_);
+
+});
+
+
 gulp.task('minify',function(){
 
-    var build_=platformStream()
+    var build_=srcStream(build)
         .pipe(concat('elliptical-platform.js'))
         .pipe(gulp.dest(src));
 
-    var minify_=releaseStream()
+    var minify_=srcStream(release)
         .pipe(concat('elliptical-platform.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(dist));
@@ -45,10 +62,22 @@ gulp.task('minify',function(){
     return merge(build_, minify_);
 });
 
-function platformStream(){
-    return gulp.src(build);
+gulp.task('minify-core',function(){
+
+    var build_=srcStream(buildCore)
+        .pipe(concat('elliptical-platform-core.js'))
+        .pipe(gulp.dest(src));
+
+    var minify_=srcStream(releaseCore)
+        .pipe(concat('elliptical-platform-core.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dist));
+
+    return merge(build_, minify_);
+});
+
+
+function srcStream(src){
+    return gulp.src(src);
 }
 
-function releaseStream(){
-    return gulp.src(release);
-}
